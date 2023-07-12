c4aee6f5a068c180b8bbaabf3125e433b333fccc
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-07-13 00:10:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,126 +1,130 @@
-/* GAME BOARD STYLES */
-#gameContainer {
-    /*
-    width and height of .gameBoardPixel should 1/40 of the width and height of #gameContainer,
-    because it is used in calculation in the jscript.js file
-    */
-    width: 40vw;
-    height: 40vw;
-    background-color: #02040c;
-   }
-   
-  
-   .gameBoardPixel {
-    /* background-color: slategrey; */
-    /*
-    width and height of .gameBoardPixel should 1/40 of the width and height of #gameContainer,
-    because it is used in calculation in the jscript.js file
-    */
-    width: 1vw;
-    height: 1vw;
-    border-radius: 8px;
-    float: left;
-   }
-   /* GAME BOARD STYLES END*/
-   
-  
-   /* SNAKE STYLES */
-   .snakeBodyPixel {
-    background-color: #4ce71d;
-   }
-   /* SNAKE STYLES END*/
-   
-  
-   /* FOOD STYLES */
-   .food {
-    background-color: #0e66ca;
-   }
-   /* FOOD STYLES END*/
-   
-  
-   /* SCORE STYLES */
-   #scoreContainer {
-    justify-content: space-around;
-   }
-   
-  
-   .scoreBoard {
-    font-size: 50px;
-    display: inline-block;
-    padding: 1vw;
-   }
-   /* SCORE STYLES END */
-   
-  
-   /* Hide #onScreenControllers on desktop */
-   #onScreenControllers {
-    display: none;
-   }
-   
-  
-   .developerDetails {
-    margin-top: 2vw;
-    display: flex;
-    flex-direction: column;
-    color: #0c1021;
-    font-family: monospace;
-   }
-   
-  
-   .developerDetails a {
-    color: #0c1021;
-   }
-   
-  
-   @media only screen and (max-width: 768px) {
-    /* MOBILE */
-    #gameContainer {
-    width: 80vw;
-    height: 80vw;
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('gameContainer');
+  const scoreElement = document.getElementById('score');
+  const gridSize = 40;
+  const pixelSize = 10;
+  const initialSnakeLength = 3;
+
+  // Create the grid
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.id = `pixel${i * gridSize + j}`;
+      container.appendChild(pixel);
     }
-   
-  
-    .gameBoardPixel {
-    width: 2vw;
-    height: 2vw;
+  }
+
+  // Create the snake
+  const snake = [];
+  for (let i = 0; i < initialSnakeLength; i++) {
+    const snakePixel = document.getElementById(`pixel${20 * gridSize + i}`);
+    snakePixel.classList.add('snakeBodyPixel');
+    snake.push(snakePixel);
+  }
+
+  // Set initial direction and movement interval
+  let direction = 'right';
+  let interval = setInterval(moveSnake, 100);
+
+  // Set initial score
+  let score = 0;
+
+  // Generate food
+  let food = generateFood();
+
+  // Move the snake
+  function moveSnake() {
+    const head = snake[snake.length - 1];
+    let newHead;
+
+    // Calculate the position of the new head based on the current direction
+    const [row, col] = getRowAndColFromId(head.id);
+    switch (direction) {
+      case 'up':
+        newHead = document.getElementById(`pixel${(row - 1) * gridSize + col}`);
+        break;
+      case 'down':
+        newHead = document.getElementById(`pixel${(row + 1) * gridSize + col}`);
+        break;
+      case 'left':
+        newHead = document.getElementById(`pixel${row * gridSize + (col - 1)}`);
+        break;
+      case 'right':
+        newHead = document.getElementById(`pixel${row * gridSize + (col + 1)}`);
+        break;
     }
-   
-  
-    #scoreContainer {
-    width: 80vw;
+
+    // Check if the new head collides with the snake's body or the game boundaries
+    if (
+      newHead === null ||
+      snake.includes(newHead) ||
+      newHead.classList.contains('snakeBodyPixel')
+    ) {
+      clearInterval(interval);
+      alert('Game Over!');
+      return;
     }
-   
-  
-    #onScreenControllers {
-    width: 80vw;
-    margin: 5vw auto;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+
+    // Add the new head to the snake
+    snake.push(newHead);
+
+    // Check if the new head collides with the food
+    if (newHead === food) {
+      // Increase the score
+      score++;
+      scoreElement.textContent = score;
+
+      // Generate new food
+      food = generateFood();
+    } else {
+      // Remove the tail of the snake
+      const tail = snake.shift();
+      tail.classList.remove('snakeBodyPixel');
     }
-   
-  
-    #onScreenControllers > div {
-    display: flex;
-    flex-direction: column;
+
+    // Update the classes of the snake's body pixels
+    for (const pixel of snake) {
+      pixel.classList.add('snakeBodyPixel');
     }
-   
-  
-    #onScreenControllers button {
-    background-color: transparent;
-    height: 20vw;
-    width: 20vw;
-    font-size: 10vw;
-    border: none;
+  }
+
+  // Generate a random food pixel
+  function generateFood() {
+    const emptyPixels = Array.from(container.getElementsByClassName('pixel')).filter(
+      pixel => !pixel.classList.contains('snakeBodyPixel')
+    );
+
+    const randomIndex = Math.floor(Math.random() * emptyPixels.length);
+    const foodPixel = emptyPixels[randomIndex];
+    foodPixel.classList.add('food');
+
+    return foodPixel;
+  }
+
+  // Handle arrow key presses to change the direction
+  document.addEventListener('keydown', event => {
+    switch (event.key) {
+      case 'ArrowUp':
+        if (direction !== 'down') direction = 'up';
+        break;
+      case 'ArrowDown':
+        if (direction !== 'up') direction = 'down';
+        break;
+      case 'ArrowLeft':
+        if (direction !== 'right') direction = 'left';
+        break;
+      case 'ArrowRight':
+        if (direction !== 'left') direction = 'right';
+        break;
     }
-   
-  
-    #onScreenControllers button:focus {
-    outline: none;
-    }
-   
-  
-    #onScreenControllers button:active {
-    background-color: slategray;
-    }
-   }
+  });
+
+  // Helper function to calculate the row and column of a pixel based on its ID
+  function getRowAndColFromId(id) {
+    const pixelIndex = parseInt(id.substring(5));
+    const row = Math.floor(pixelIndex / gridSize);
+    const col = pixelIndex % gridSize;
+    return [row, col];
+  }
+});
